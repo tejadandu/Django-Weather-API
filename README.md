ac2bab49466cf08f8556b2fd672e136b4ad4a22a
Teja Dandu <teja.dandu484@gmail.com>
2022-11-10 00:22:31 +0530

    Added weather api

---
+++ README.md
@@ -0,0 +1,9 @@
+## Weather detector API call with the city name of a country
+
+- `python3 -m venv <weather-app>` ---> creating the python environment
+- `source <weather-app>/bin/activate` ---> activate the enviroment
+- `python3 install -r requirements.txt` ---> to install the Django
+- `python3 manage.py runserver` ----> to start running the django server
+
+
+
---
+++ weatherdetector/weather/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+import json
+import urllib.request
+
+# Create your views here.
+
+
+def index(request):
+    if request.method == "POST":
+        city = request.POST["city"]
+        res = urllib.request.urlopen(
+            "http://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&appid=62f26abc406e1cb3e550faabc22afc37"
+        ).read()
+        json_data = json.loads(res)
+        data = {
+            "country_code": str(json_data["sys"]["country"]),
+            "coordinate": str(json_data["coord"]["lon"])
+            + " "
+            + str(json_data["coord"]["lat"]),
+            "temp": str(json_data["main"]["temp"]) + "k",
+            "pressure": str(json_data["main"]["pressure"]),
+            "humidity": str(json_data["main"]["humidity"]),
+        }
+
+    else:
+        city = ""
+        data = {}
+    return render(request, "index.html", {"city": city, "data": data})
